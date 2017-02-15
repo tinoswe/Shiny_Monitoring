de2b5ecbf31e63053bd09a49bf76c96d69a6f31b
tinoswe <olivomartino@gmail.com>
2017-02-15 09:25:27 +0100

    .

--- server.R
+++ server.R
@@ -1,6 +1,8 @@
 library(shiny)
 #install.packages("lubridate")
 library(lubridate)
+library(dygraphs)
+library(xts)
 
 df <- read.csv(file="../RH_T/CSV_Files/all_data.csv",
                header=TRUE,
@@ -59,6 +61,17 @@ df_feb16$DateTime <- c()
 df_feb16 <- df_feb16[,c(9,10,1,2,3,4,5,6,7,8)]
 df_feb16 <- na.omit(df_feb16)
 
+#mar16
+df_mar16 <- df[month(df$DateTime)=="3",] 
+df_mar16$Day <- as.character(date(df_mar16$DateTime))
+df_mar16$Time <- paste(sprintf("%02d", hour(df_mar16$DateTime)),
+                       sprintf("%02d", minute(df_mar16$DateTime)),
+                       sprintf("%02d", second(df_mar16$DateTime)),
+                       sep=":")
+df_mar16$DateTime <- c()
+df_mar16 <- df_mar16[,c(9,10,1,2,3,4,5,6,7,8)]
+df_mar16 <- na.omit(df_mar16)
+
 #df_jan16 <- df_jan16[1:100,]
 #df_feb16 <- df_feb16[1:100,]
 
@@ -68,13 +81,16 @@ function(input, output) {
     
     switch(input$dataset,
            "Jan 16" = df_jan16,
-           "Feb 16" = df_feb16)
+           "Feb 16" = df_feb16,
+           "Mar 16" = df_mar16)
     
   })
   
   output$plot <- renderPlot({
+
     library(ggplot2)
     library(reshape)
+    library(dygraphs)
     
     data <- datasetInput()
     
@@ -100,16 +116,28 @@ function(input, output) {
            aes(x = tt,
                y = xx)) + geom_line(aes(colour = fact)) + theme_bw()
            
-    
-    #plot(d_t_t$dtime,
-    #     d_t_t$value) 
-       
+
   })
   
   output$table <- renderDataTable( #{
     dataset <- datasetInput(),
     options = list(pageLength = 25,
-                   dom  = 'tip')
+                   dom  = 'tip'),
+    
+    
+  output$dygraph <- renderDygraph({
+    
+    data <- datasetInput()
+    filtered <- filter(data,
+                       Variety == input$productname,
+                       Count == input$count )
+    
+    #dygraph(datasetInput(), main="") %>%
+    #  dySeries(c("lwr", "fit", "upr"), label="" ) 
+    
+    }
+  )
+    
 #  }
 )
   
--- ui.R
+++ ui.R
@@ -1,22 +1,31 @@
 fluidPage(
-  titlePanel('MOB: T and HR'),
+  titlePanel('Monitoraggio temperatura e umidità'),
   sidebarLayout(
     
     sidebarPanel(
     
       selectInput("dataset", "Choose a dataset:",
                   choices = c("Jan 16",
-                              "Feb 16")
+                              "Feb 16",
+                              "Mar 16")
       #,downloadButton('downloadData', 'Download')
-      
-    )
+    ),
+    
+    dateRangeInput("daterange1", "Date range:",
+                   start = "2016-01-01",
+                   end   = "2016-12-31"),
+    
+       actionButton("update", "Update range")
+    
+    
     ),
     
     mainPanel(
       tabsetPanel(
       
         tabPanel("Data", dataTableOutput('table')),
-        tabPanel("Charts", plotOutput("plot"))
+        tabPanel("Charts", plotOutput("plot")),
+        tabPanel("DyChart",dygraphOutput("dygraph"))
         
         )
     )
